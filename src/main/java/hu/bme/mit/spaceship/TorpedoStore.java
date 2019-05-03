cf31a440135e22836adf978ae9167ec215b4fd60
Markerer <tolesz11@windowslive.com>
2019-05-03 04:09:35 -0700

    -= javitasa

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -42,7 +42,7 @@ package hu.bme.mit.spaceship;
 	    
 	    if (r >= FAILURE_RATE) {
 	      // successful firing
-	      this.torpedoCount =- numberOfTorpedos;
+	      this.torpedoCount -= numberOfTorpedos;
 	      success = true;
 	    } else {
 	      // simulated failure
